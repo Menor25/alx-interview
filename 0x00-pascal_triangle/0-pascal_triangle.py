deec0ba390904aae0c432e482c47c2b1c8586c97
Menor25 <ajirimenor@gmail.com>
2022-12-26 09:33:13 +0100

    Creating a pascal triangle in python using loop

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defining the function for the pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal’s
+    triangle of n.
+    Arguments:
+        n (int): Height of the triangle.
+    """
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(i - 1):
+                row.append(triangle[i-1][j] + triangle[i-1][j+1])
+            row.append(1)
+            triangle.append(row)
+    return triangle
+    
