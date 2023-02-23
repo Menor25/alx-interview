fea351f451a682f0e0660a447da96b3214cc6670
Menor25 <ajirimenor@gmail.com>
2023-02-24 00:01:44 +0100

    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,16 +10,17 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    if coins is None:
+    if len(coins) is 0:
         return -1
-
-    for coin in sorted(coins, reverse=True):
-        for cn in sorted(coins, reverse=True):
-            sum_ = coin
-            count = 1
-            while sum_ <= total + 1:
-                if sum_ == total:
-                    return count
-                sum_ += cn
-                count += 1
-    return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
